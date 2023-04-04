868d03e2e24a3ded8a7aeda69d8c3e7656f53e59
Khairuzzaman <kzamanbm@gmail.com>
2023-04-04 14:18:55 +0800

    Updated script.js

--- script.js
+++ script.js
@@ -22,8 +22,21 @@ function startQuiz() {
       clearInterval(myInterval);
       clearTimeout(myInterval);
       time = 0;
+      ab1.disabled = true;
+      ab2.disabled = true;
+      ab3.disabled = true;
+      ab4.disabled = true;
     }
   }
+
+  scoreBox.style.visibility = "visible";
+  timerBox.style.visibility = "visible";
+  nextButton.style.visibility = "visible";
+  questionBox.style.visibility = "visible";
+  document.querySelector("#answers").style.visibility = "visible";
+  startButton.style.visibility = "hidden";
+
+  nextButton.innerHTML = "Skip";
 }
 
 // Next button
@@ -32,6 +45,10 @@ nextButton.addEventListener("click", nextQn);
 function nextQn() {
   console.log("Next question");
   currentQuestion++;
+  if (currentQuestion == questions.length) {
+    nextButton.style.visibility = "hidden";
+    return;
+  }
   questionBox.innerHTML = randomQns[currentQuestion].question;
   ab1.innerHTML = randomQns[currentQuestion].answers[0];
   ab2.innerHTML = randomQns[currentQuestion].answers[1];
@@ -49,6 +66,24 @@ function nextQn() {
   ab4.disabled = false;
   // let time = 10;
   // updateCountdown();
+
+  nextButton.innerHTML = "Skip";
+
+  let time = 10;
+  let myInterval = setInterval(updateCountdown, 1000);
+  function updateCountdown() {
+    timerBox.innerHTML = time;
+    time--;
+    if (time < 0) {
+      clearInterval(myInterval);
+      clearTimeout(myInterval);
+      time = 0;
+      ab1.disabled = true;
+      ab2.disabled = true;
+      ab3.disabled = true;
+      ab4.disabled = true;
+    }
+  }
 }
 
 ab1.addEventListener("click", function (e) {
@@ -64,6 +99,18 @@ ab1.addEventListener("click", function (e) {
   ab2.disabled = true;
   ab3.disabled = true;
   ab4.disabled = true;
+
+  if (ab2.innerHTML == randomQns[currentQuestion].correct) {
+    ab2.style.background = "LawnGreen";
+  }
+  if (ab3.innerHTML == randomQns[currentQuestion].correct) {
+    ab3.style.background = "LawnGreen";
+  }
+  if (ab4.innerHTML == randomQns[currentQuestion].correct) {
+    ab4.style.background = "LawnGreen";
+  }
+
+  nextButton.innerHTML = "Next";
 });
 
 ab2.addEventListener("click", function (e) {
@@ -78,6 +125,18 @@ ab2.addEventListener("click", function (e) {
   ab1.disabled = true;
   ab3.disabled = true;
   ab4.disabled = true;
+
+  if (ab1.innerHTML == randomQns[currentQuestion].correct) {
+    ab1.style.background = "LawnGreen";
+  }
+  if (ab3.innerHTML == randomQns[currentQuestion].correct) {
+    ab3.style.background = "LawnGreen";
+  }
+  if (ab4.innerHTML == randomQns[currentQuestion].correct) {
+    ab4.style.background = "LawnGreen";
+  }
+
+  nextButton.innerHTML = "Next";
 });
 
 ab3.addEventListener("click", function (e) {
@@ -92,6 +151,18 @@ ab3.addEventListener("click", function (e) {
   ab1.disabled = true;
   ab2.disabled = true;
   ab4.disabled = true;
+
+  if (ab1.innerHTML == randomQns[currentQuestion].correct) {
+    ab1.style.background = "LawnGreen";
+  }
+  if (ab2.innerHTML == randomQns[currentQuestion].correct) {
+    ab2.style.background = "LawnGreen";
+  }
+  if (ab4.innerHTML == randomQns[currentQuestion].correct) {
+    ab4.style.background = "LawnGreen";
+  }
+
+  nextButton.innerHTML = "Next";
 });
 
 ab4.addEventListener("click", function (e) {
@@ -106,28 +177,40 @@ ab4.addEventListener("click", function (e) {
   ab1.disabled = true;
   ab2.disabled = true;
   ab3.disabled = true;
+
+  if (ab1.innerHTML == randomQns[currentQuestion].correct) {
+    ab1.style.background = "LawnGreen";
+  }
+  if (ab2.innerHTML == randomQns[currentQuestion].correct) {
+    ab1.style.background = "LawnGreen";
+  }
+  if (ab3.innerHTML == randomQns[currentQuestion].correct) {
+    ab1.style.background = "LawnGreen";
+  }
+
+  nextButton.innerHTML = "Next";
+
+  // function checkCorrect(button, correct)
+
+  // button.innerHTML == correct
 });
+
+// answers.forEach((element) => {
+//   if (element.innerHTML == randomQns[currentQuestion].correct) {
+
+//   }
+// });
 
 // Score
 let score = 0;
 let scoreBox = document.getElementById("score");
 function updateScore() {
-  scoreBox.innerHTML = score;
+  scoreBox.innerHTML = score + "/30";
 }
 
 // Timer;
 let time = 10;
 let timerBox = document.getElementById("timer");
-// let myInterval = setInterval(updateCountdown, 1000);
-// function updateCountdown() {
-//   timerBox.innerHTML = time;
-//   time--;
-//   if (time < 0) {
-//     clearInterval(myInterval);
-//     clearTimeout(myInterval);
-//     time = 0;
-//   }
-// }
 
 // Questions
 let questionBox = document.getElementById("question");
@@ -148,6 +231,34 @@ const questions = [
     answers: ["7", "10", "5", "6"],
     correct: "10",
   },
+  {
+    question: "What is 5+5?",
+    answers: ["7", "10", "5", "6"],
+    correct: "10",
+  },
+  {
+    question: "What is 5+5?",
+    answers: ["7", "10", "5", "6"],
+    correct: "10",
+  },
+  {
+    question: "What is 5+5?",
+    answers: ["7", "10", "5", "6"],
+    correct: "10",
+  },
+  {
+    question: "What is 5+5?",
+    answers: ["7", "10", "5", "6"],
+    correct: "10",
+  },
 ];
 
 let randomQns = questions.sort(() => Math.random() - 0.5);
+
+window.onload = (event) => {
+  scoreBox.style.visibility = "hidden";
+  timerBox.style.visibility = "hidden";
+  nextButton.style.visibility = "hidden";
+  questionBox.style.visibility = "hidden";
+  document.querySelector("#answers").style.visibility = "hidden";
+};
